b67119dfcbf981ed8ccf11f6b17c00c980a993fe
anton <antonovechkino4@gmail.com>
2024-11-29 21:37:09 +0300

    fix

---
+++ HotelManager.Data/Repositories/Room/RoomRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelManager.Data.Component;
+
+namespace HotelManager.Data.Repositories.Room
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private List<RoomDto> roomDtos = new List<RoomDto>
+        {
+                new RoomDto { Id = 0, Number = 0 },
+                new RoomDto { Id = 1, Number = 1 },
+                new RoomDto { Id = 2, Number = 2 },
+                new RoomDto { Id = 3, Number = 3 },
+                new RoomDto { Id = 4, Number = 4 }
+        };
+
+        public RoomDto GetById(int id)
+        {
+            return roomDtos.FirstOrDefault(r => r.Id == id);
+        }
+        public List<RoomDto> Get()
+        {
+            return roomDtos;
+        }
+        public void Update(RoomDto roomDto)
+        {
+            for (int i = 0; i < roomDtos.Count; i++)
+            {
+                if (roomDto.Id == roomDtos[i].Id)
+                {
+                    roomDtos[i] = roomDto;
+                    break;
+                }               
+            }
+        }
+        public void Delete(int id)
+        {
+            roomDtos.RemoveAll(r => r.Id == id);
+        }
+        public void Create(RoomDto roomDto)
+        {
+            roomDtos.Add(roomDto);
+        }
+    }
+}
--- LearnBack/Controllers/RoomController.cs
+++ LearnBack/Controllers/RoomController.cs
@@ -3,31 +3,37 @@ using HotelManager.Domain.Services.Room;
 using HotelManager.Data.Component;
 namespace LearnBack.Controllers
 {
-    public class RoomController : Controller
+    [ApiController]
+    public class RoomController : ControllerBase
     {
         private readonly IRoomService _service;
         public RoomController(IRoomService service)
         {
             _service = service;
         }
+        [HttpGet("GetById")]
         public RoomDto GetById(int id)
         {
             return _service.GetById(id);
         }
+        [HttpGet("Get")]
         public List<RoomDto> Get()
         {
             return _service.Get();
         }
+        [HttpPatch("Update")]
         public void Update(RoomDto roomDto)
         {
             _service.Update(roomDto);
         }
+        [HttpDelete("Delete")]
         public void Delete(int id)
         {
             _service.Delete(id);
         }
+        [HttpPost("Create")]
         public void Create(RoomDto roomDto)
-        {  
+        {
             _service.Create(roomDto);
         }
     }
